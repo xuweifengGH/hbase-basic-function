99025bad18037b11fe37394f3d01ecef624b99d7
weifeng xu <xuweifeng2016@outlook.com>
2020-07-06 16:42:59 +0800

    update

--- src/main/com/weifeng/hbase/federation/counter/IncrementSingleExample.java
+++ src/main/com/weifeng/hbase/federation/counter/IncrementSingleExample.java
@@ -23,18 +23,18 @@ public class IncrementSingleExample {
     helper.createTable("testtable", "daily");
     Connection connection = ConnectionFactory.createConnection(hConf);
     Table table = connection.getTable(TableName.valueOf("testtable"));
-    // vv IncrementSingleExample
-    long cnt1 = table.incrementColumnValue(Bytes.toBytes("20110101"), // co IncrementSingleExample-1-Incr1 Increase counter by one.
+
+    long cnt1 = table.incrementColumnValue(Bytes.toBytes("20110101"),
       Bytes.toBytes("daily"), Bytes.toBytes("hits"), 1);
-    long cnt2 = table.incrementColumnValue(Bytes.toBytes("20110101"), // co IncrementSingleExample-2-Incr2 Increase counter by one a second time.
+    long cnt2 = table.incrementColumnValue(Bytes.toBytes("20110101"),
       Bytes.toBytes("daily"), Bytes.toBytes("hits"), 1);
 
-    long current = table.incrementColumnValue(Bytes.toBytes("20110101"), // co IncrementSingleExample-3-GetCurrent Get current value of the counter without increasing it.
+    long current = table.incrementColumnValue(Bytes.toBytes("20110101"),
       Bytes.toBytes("daily"), Bytes.toBytes("hits"), 0);
 
-    long cnt3 = table.incrementColumnValue(Bytes.toBytes("20110101"), // co IncrementSingleExample-4-Decr1 Decrease counter by one.
+    long cnt3 = table.incrementColumnValue(Bytes.toBytes("20110101"),
       Bytes.toBytes("daily"), Bytes.toBytes("hits"), -1);
-    // ^^ IncrementSingleExample
+
     System.out.println("cnt1: " + cnt1 + ", cnt2: " + cnt2 +
       ", current: " + current + ", cnt3: " + cnt3);
   }
